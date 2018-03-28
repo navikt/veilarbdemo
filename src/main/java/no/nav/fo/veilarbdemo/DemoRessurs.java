94c099ec230255b65a4c27f6aa84f21afa801b1d
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-03-28 09:22:16 +0200

    tester eksponering via apigw

--- src/main/java/no/nav/fo/veilarbdemo/DemoRessurs.java
+++ src/main/java/no/nav/fo/veilarbdemo/DemoRessurs.java
@@ -3,6 +3,7 @@ package no.nav.fo.veilarbdemo;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Component
@@ -11,7 +12,19 @@ public class DemoRessurs {
 
     @GET
     public String get() {
+        return ok();
+    }
+
+    @GET
+    @Path("/ok")
+    public String ok() {
         return "alt ok!";
+    }
+
+    @POST
+    @Path("/pipe")
+    public String pipe(String content) {
+        return content;
     }
 
     @GET
