03030ab56130d8a1999a878dc5dccbd5297af63b
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-06-25 17:47:37 +0200

    la til treigt endepunkt for test-formål

--- src/main/java/no/nav/fo/veilarbdemo/DemoRessurs.java
+++ src/main/java/no/nav/fo/veilarbdemo/DemoRessurs.java
@@ -1,13 +1,17 @@
 package no.nav.fo.veilarbdemo;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.time.Duration;
 
 @Component
 @Path("/")
+@Slf4j
 public class DemoRessurs {
 
     @GET
@@ -18,6 +22,16 @@ public class DemoRessurs {
     @GET
     @Path("/ok")
     public String ok() {
+        return "alt ok!";
+    }
+
+    @GET
+    @Path("/slow")
+    @SneakyThrows
+    public String slow() {
+        long millis = Duration.ofMinutes(5).toMillis();
+        log.info("sleeping {}ms", millis);
+        Thread.sleep(millis);
         return "alt ok!";
     }
 
