9b01383427daf07fe44a9b2e19169eab5d8f9eab
Alexander Gård <alex.gaard@hotmail.com>
2020-03-13 17:32:57 +0100

    Fix health checks og fjern leader-election

--- src/main/java/no/nav/veilarbdemo/controller/HealthController.java
+++ src/main/java/no/nav/veilarbdemo/controller/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/internal/health")
+@RequestMapping("/internal")
 public class HealthController {
 
     @GetMapping("/isAlive")
