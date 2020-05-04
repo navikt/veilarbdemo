7799f30af069b3d3f6b8674a5c8d7d2e9a6c6c67
Alexander Gård <alex.gaard@hotmail.com>
2020-05-04 15:11:49 +0200

    Test med setup truststore

--- src/main/java/no/nav/veilarbdemo/VeilarbdemoApp.java
+++ src/main/java/no/nav/veilarbdemo/VeilarbdemoApp.java
@@ -3,11 +3,18 @@ package no.nav.veilarbdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static no.nav.common.utils.EnvironmentUtils.getOptionalProperty;
+
 
 @SpringBootApplication
 public class VeilarbdemoApp {
 
     public static void main(String[] args) {
+        if (!getOptionalProperty("javax.net.ssl.trustStore").isPresent()) {
+            getOptionalProperty("NAV_TRUSTSTORE_PATH").ifPresent(path -> System.setProperty("javax.net.ssl.trustStore", path));
+            getOptionalProperty("NAV_TRUSTSTORE_PASSWORD").ifPresent(passwd -> System.setProperty("javax.net.ssl.trustStorePassword", passwd));
+        }
+
         SpringApplication.run(VeilarbdemoApp.class, args);
     }
 
