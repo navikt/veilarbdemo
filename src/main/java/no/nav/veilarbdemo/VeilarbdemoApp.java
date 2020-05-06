772bb641de09be01de35e1fc70d754021157c86f
Alexander Gård <alex.gaard@hotmail.com>
2020-05-06 15:35:10 +0200

    Test ny rest client

--- src/main/java/no/nav/veilarbdemo/VeilarbdemoApp.java
+++ src/main/java/no/nav/veilarbdemo/VeilarbdemoApp.java
@@ -1,5 +1,9 @@
 package no.nav.veilarbdemo;
 
+import no.nav.veilarbdemo.client.RestClient;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +14,30 @@ import static no.nav.common.utils.EnvironmentUtils.getOptionalProperty;
 public class VeilarbdemoApp {
 
     public static void main(String[] args) {
+        System.setProperty("java.net.useSystemProxies", "true");
         if (!getOptionalProperty("javax.net.ssl.trustStore").isPresent()) {
             getOptionalProperty("NAV_TRUSTSTORE_PATH").ifPresent(path -> System.setProperty("javax.net.ssl.trustStore", path));
             getOptionalProperty("NAV_TRUSTSTORE_PASSWORD").ifPresent(passwd -> System.setProperty("javax.net.ssl.trustStorePassword", passwd));
+        }
+
+        Request openAmRequest = new Request.Builder()
+                .url("https://isso-q.adeo.no/isso/oauth2/.well-known/openid-configuration")
+                .build();
+
+        Request azureAdRequest = new Request.Builder()
+                .url("https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/.well-known/openid-configuration")
+                .build();
+
+        try {
+            OkHttpClient client = RestClient.baseClient();
+
+            Response openAmResponse = client.newCall(openAmRequest).execute();
+            System.out.println(openAmResponse.body().string());
+
+            Response azureAdResponse = client.newCall(azureAdRequest).execute();
+            System.out.println(azureAdResponse.body().string());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         SpringApplication.run(VeilarbdemoApp.class, args);
